f273aadb989f4630cd6d506c8674106e204cd050
amirbaum <colocba@gmail.com>
2019-07-06 12:43:44 +0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxPageScrollModule } from 'ngx-page-scroll';
 
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
+import { NavbarComponent } from './navbar/navbar.component';
+import { WhatWeDoComponent } from './what-we-do/what-we-do.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
+import { CoverPictureComponent } from './cover-picture/cover-picture.component';
+import { ForWhoComponent } from './for-who/for-who.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    WhatWeDoComponent,
+    HowItWorksComponent,
+    CoverPictureComponent,
+    ForWhoComponent,
+    ContactUsComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxPageScrollModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
