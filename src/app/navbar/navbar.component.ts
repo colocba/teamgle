96d511c84502e1c9cd23e58c7392bca6dc10cc9b
amirbaum <colocba@gmail.com>
2019-07-12 13:29:05 +0300

    Fixing scroll on multiple screen sizes

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -10,16 +10,33 @@ export class NavbarComponent implements OnInit {
 
   private config: ScrollToConfigOptions;
 
+  /* Each time any a is pressed need to set dynamically offset deppending
+      on screen size */
+  private desktopOffset = -140;
+  private phonePortraitOffset = -400;
+  private phoneLandscapeOffset = -150;
+
   constructor(private _scrollToService: ScrollToService) {
   }
   
   public triggerScrollTo(divId) {
+    const currentOffset = this.setOffsetByScreenSize();
     this.config = {
       target: divId,
       duration: 650,
-      offset: -140
+      offset: currentOffset
     };
     this._scrollToService.scrollTo(this.config);
+  }
+
+  public setOffsetByScreenSize() {
+    if (screen.width <= 450) {
+      return this.phonePortraitOffset;
+    }
+    if (screen.width <= 850) {
+      return this.phoneLandscapeOffset;
+    }
+    return this.desktopOffset;
   }
 
   ngOnInit() {
