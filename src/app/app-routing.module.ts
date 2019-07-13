f1eda968f16b6d8fcf820c099c53dda917c0f677
amirbaum <colocba@gmail.com>
2019-07-13 12:16:23 +0300

    Fixing routing on 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes, {scrollPositionRestoration: 'top', useHash:true}) ],
+  imports: [ RouterModule.forRoot(routes, {scrollPositionRestoration: 'top'}) ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
