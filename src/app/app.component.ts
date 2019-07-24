ba26da90cbf0d0c4f19e4e8f89e0846572dc1f2f
amirbaum <colocba@gmail.com>
2019-07-24 03:25:37 +0300

    bug on phone when changing effects on scroll in navbar - effects removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,14 @@ import * as $ from 'jquery';
 })
 export class AppComponent implements OnInit {
   ngOnInit(): void {
-    // Changing the navbar style on scroll
-    $(window).scroll(function() {
-      $('nav').toggleClass('scrolled', $(this).scrollTop() > 50)
-      $('.cov-img').toggleClass('scrolled1', $(this).scrollTop() > 50)
-      $('.navbar').toggleClass('downsized', $(this).scrollTop() > 50)
-    })
+    // Changing the navbar style on scroll only if we are in desktop mode
+    if (screen.width > 800) {
+      $(window).scroll(function() {
+        $('nav').toggleClass('scrolled', $(this).scrollTop() > 50)
+        $('.cov-img').toggleClass('scrolled1', $(this).scrollTop() > 50)
+        $('.navbar').toggleClass('downsized', $(this).scrollTop() > 50)
+      })
+    }
   }
   title = 'Teamgle';
 }
